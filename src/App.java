b2b5c082779804c0a9fe716f03416339ed47ff43
Synclair Wang <synclairwang11@yahoo.com>
2018-03-10 02:24:40 -0500

    added rock paper scissors scene

--- src/App.java
+++ src/App.java
@@ -6,43 +6,77 @@ public class App extends PApplet{
     PFont font;
     Game game = new Game();
     MainMenu menu = new MainMenu();
+    RPS rps = new RPS();
 
     public static void main(String[] args) {
         PApplet.main("App", args);
     }
 
     public void settings() {
-        size(500, 500);
+        size(700, 500);
     }
 
     public void setup() {
         font = createFont("Arial", 25, true);
-        stroke(0);
+//        stroke(0);
+        noStroke();
         fill(0);
     }
 
     public void draw() {
         int currentScene = game.getScene();
 
-        if (currentScene == 0) { // DRAW MENU
+        if (currentScene == 0) { // MAIN MENU
             background(43, 45, 66);
-            fill(237, 242, 244);
-            textFont(font, 60);
+            fill(46, 196, 182);
+            textFont(font, 100);
             textAlign(RIGHT);
-            text("ROCK PAPER", 480, 70);
-            text("TAC TOE",  480, 130);
+            text("ROCK PAPER", 680, 90);
+            text("TAC TOE",  680, 180);
 
-            int posX = 70;
+            int posX = 80;
             int posY = 380;
             for (int i = 0; i < menu.getButtons().length; i++) {
+                textFont(font, 70);
                 if (i == menu.getSelected()) {
+                    fill(239, 35, 60);
                     textAlign(LEFT);
-                    text(">", posX - 40, posY);
+                    text(">", posX - 50, posY);
                 }
+                fill(237, 242, 244);
                 textAlign(LEFT);
                 text(menu.getButtons()[i], posX, posY);
-                posY += 60;
+                posY += 70;
             }
+        } else if (currentScene == 1) { // RPS
+            background(250, 121, 33);
+            fill(253, 231, 76);
+            textFont(font, 80);
+            text("PICK ONE", 170, 100);
+            strokeWeight(5);
+            stroke(253, 231, 76);
+            line(0, 140, 700, 140);
+            noStroke();
+            int posX = 35;
+            int posY = 220;
+            for (int i = 0; i < rps.getChoices().length; i++) {
+
+                noStroke();
+                fill(155, 197, 61);
+                rect(posX, posY, 190, 190);
+                textFont(font, 30);
+                fill(0);
+                text(rps.getChoices()[i], posX + 50, posY + 100);
+
+                if (i == rps.getSelected()) {
+                    noFill();
+                    strokeWeight(5);
+                    stroke(0);
+                    rect(posX - 5, posY - 5, 200, 200);
+                }
+                posX += 220;
+            }
+        } else if (currentScene == 2) {// RPS RESULTS
 
         }
     }
@@ -60,6 +94,16 @@ public class App extends PApplet{
             } else if (key == ' ' || key == ENTER || key == RETURN) {
                 if (menu.getSelected() == 0) { game.setScene(1); }
                 else { exit(); }
+            }
+        } else if (currentScene == 1) {
+            if (key == CODED) {
+                if (keyCode == LEFT) {
+                    rps.changeSelected(-1);
+                } else if (keyCode == RIGHT) {
+                    rps.changeSelected(1);
+                }
+            } else if (key == ' ' || key == ENTER || key == RETURN) {
+                game.setScene(2);
             }
         }
 
--- src/RPS.java
+++ src/RPS.java
@@ -1,6 +1,19 @@
 public class RPS {
 
+    private String[] choices = {"Rock", "Paper", "Scissor"};
+    private int selected = 0;
+
     public void play() {
 
+    }
+
+    public String[] getChoices() { return choices; }
+
+    public int getSelected() { return selected; }
+
+    public void changeSelected(int num) {
+        selected += num;
+        if (selected == -1) { selected = 1; }
+        if (selected == 3) { selected = 0; }
     }
 }
